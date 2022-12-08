23b2f31af1423e6ec96dd066657e51de834eca18
TORIEN <105123575+Torien-TORIEN@users.noreply.github.com>
2022-12-08 16:41:34 +0100

    Add files via upload

---
+++ src/projet/Controller/AdminController.java
@@ -0,0 +1,17 @@
+
+package projet.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class AdminController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
